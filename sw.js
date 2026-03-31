81bfad7b5e2bf0827e28d65e5eb8ef5ba7080e9b
rpropp30 <67614708+rpropp30@users.noreply.github.com>
2026-03-30 20:10:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'southern-sayings-v4';
+const CACHE_NAME = 'southern-sayings-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
